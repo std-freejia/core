03f210957322a8a483cae71a527333b1938c5f50
std-freejia <std-freejia@gmail.com>
2021-06-07 23:22:19 +0900

    객체 지향 원리 적용 - AppConfig 리팩터링 (중복 제거 - 역할에 따른 구현을 분명하게 표현하자) new MemoryMemberRepository() 의 중복 제거하자. '구체 클래스 선택'과 '의존관계 주입'을 구분했다. 애플리케이션 전체 구성을 한 눈에 파악할 수 있도록 리팩토링 했다.

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -15,19 +17,28 @@ import hello.core.order.OrderServiceImpl;
  * 생성한 객체 인스턴스의 참조를 "생성자를 통해 주입(연결)" 해준다.
  * MemberServiceImpl -> MemoryMemberRepository
  * OrderServiceImpl -> MemoryMemberRepository, FixDiscountPolicy
+ *
+ * 생성자를 통해 구현체를 선택할 수 있도록 한다!
+ * DIP의 완성 : MemberServiceImpl은 MemberRepository인 추상에만 의존하면 된다. 구체 클래스를 몰라도 된다!
  */
 public class AppConfig { // 공연 기획자의 역할을 한다.
 
     public MemberService memberService(){ // 객체의 생성과 연결을 모두 담당한다.
-        // 생성자를 통해 구현체를 선택할 수 있도록 한다!
-        // DIP의 완성 : MemberServiceImpl은 MemberRepository인 추상에만 의존하면 된다. 구체 클래스를 몰라도 된다!
 
-        return new MemberServiceImpl(new MemoryMemberRepository()); // 의존 관계 주입 (Dependency Injection)
+        return new MemberServiceImpl(memberRepository()); // 의존 관계 주입 (Dependency Injection)
+    }
+
+    private MemberRepository memberRepository() {  // 객체 생성의 중복을 제거!
+        return new MemoryMemberRepository(); // 구체 클래스 생성
     }
 
     public OrderService orderService(){
         //MemberRepository 와 DiscountPolicy가 필요하다.
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();  // 구체 클래스 생성 -> '구체 클래스 선택'과 '의존관계 주입'을 구분했다.  
     }
 
 }
