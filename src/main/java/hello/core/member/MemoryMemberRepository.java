3bce32191f20d86fa8243f5937a9627ab84ab4a5
std-freejia <std-freejia@gmail.com>
2021-06-07 15:45:57 +0900

    회원 도메인 개발

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,22 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    // 참고. 실무에서는 동시성 문제 때문에 ConcurrentHashMap 를 써야 합니다.
+    private static Map<Long, Member> store = new HashMap<>();
+
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
